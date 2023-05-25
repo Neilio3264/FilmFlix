36c4a6b91eb2bdcffdbd7892f6665986b4505998
Neilio3264 <neilrathod12@gmail.com>
2023-05-24 22:08:46 -0500

    Fixed file extension syntax

--- database/packer/start.sh
+++ database/packer/start.sh
@@ -3,7 +3,7 @@
 function buildImage {
     cloud=$2
     export MACHINE_TYPE=$1
-    /usr/bin/packer build -force filmflix_${cloud}_ec2_config.json
+    /usr/bin/packer build -force filmflix_${cloud}_ec2_config.pkr.hsl
 }
 
 buildImage mongodb aws
