7763e72ceb9e950c67628a800ed269d76fcae1b6
Neilio3264 <neilrathod12@gmail.com>
2023-05-24 22:09:32 -0500

    Spelling mistake

--- database/packer/start.sh
+++ database/packer/start.sh
@@ -3,7 +3,7 @@
 function buildImage {
     cloud=$2
     export MACHINE_TYPE=$1
-    /usr/bin/packer build -force filmflix_${cloud}_ec2_config.pkr.hsl
+    /usr/bin/packer build -force filmflix_${cloud}_ec2_config.pkr.hcl
 }
 
 buildImage mongodb aws
